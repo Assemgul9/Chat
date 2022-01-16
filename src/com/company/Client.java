cb93770335d1bfcad5698b949c1400cb7e7dbeb3
Assemgul <assemgul9@mail.ru>
2022-01-16 22:39:30 +0600

    Initial commit

---
+++ src/com/company/Client.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ src/com/company/TestServer.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TestServer {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
